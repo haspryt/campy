6ff5dea0e6bea77752bb58fe8fe449be05f70f2b
haspryt <haspryt@riseup.net>
2022-03-05 22:54:14 +0200

    started work on tokenizer

---
+++ tokenizer.py
@@ -0,0 +1,30 @@
+import re
+
+def tokenize(string: str):
+    all_tokens = []
+    token_rex = [
+        re.compile(r"^[a-zA-Z_][a-zA-Z0-9_]*"),
+        re.compile(r"^[0-9]+"),
+        re.compile(r"^<<"),
+        re.compile(r"^>="),
+        re.compile(r"^<="),
+        re.compile(r"^[+*/:,><-]"),
+        re.compile(r"^[()]"),
+        re.compile(r"^[\"]"),
+        re.compile(r"^\n"),
+        re.compile(r"^="),
+    ]
+
+    while len(string):
+        string = string.lstrip(" ")
+        match = False
+
+        for token_re in token_rex:
+            mo = token_re.match(string)
+            if mo:
+                match = True
+                token = mo.group(0)
+                all_tokens.append(token)
+                string = token_re.sub('', string)
+                break
+    return all_tokens
