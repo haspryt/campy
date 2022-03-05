9a7120d893483d0ced7784a70d8549f5fe772b79
ai-z-b <aissa.zenaida@pm.me>
2022-03-06 00:31:26 +0200

    add proper string support in tokeniser

--- tokenizer.py
+++ tokenizer.py
@@ -9,15 +9,13 @@ def tokenize(string: str):
         re.compile(r"^>="),
         re.compile(r"^<="),
         re.compile(r"^//"),
-        re.compile(r"^[?!.@^~`'_|\/#$&{}+*:,><-]"),
-        re.compile(r"^[()]"),
+        re.compile(r"^[?!.@^~`'_|\/#$&{}+*:,><=()-]"),
         re.compile(r"^[\"]"),
         re.compile(r"^\n"),
-        re.compile(r"^="),
+        re.compile(r"^ "),
     ]
 
     while len(string):
-        string = string.lstrip(" ")
         match = False
 
         for token_re in token_rex:
@@ -28,4 +26,27 @@ def tokenize(string: str):
                 all_tokens.append(token)
                 string = token_re.sub('', string)
                 break
-    return all_tokens
+    
+    index = 0
+    saved_index = 0
+    ap_counter = 0
+    while True:
+        try:
+            token = all_tokens[index]
+        except:
+            all_tokens = list(filter((" ").__ne__, all_tokens))
+            return all_tokens
+        if token == "\"":
+            ap_counter += 1
+            if ap_counter % 2 == 1:
+                saved_index = index
+            else:
+                new_t = ""
+                for i in range(saved_index, index+1):
+                    new_t = new_t + ''.join(all_tokens[i])
+                for _ in range(saved_index, index):
+                    del all_tokens[saved_index]
+                all_tokens[saved_index] = new_t
+                index = saved_index + 1
+                pass
+        index += 1
