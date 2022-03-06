7d033a369ab533a0882544565e5ee3edfb37a9b1
haspryt <haspryt@riseup.net>
2022-03-06 14:52:33 +0200

    started work on tree function

---
+++ form_tree.py
@@ -0,0 +1,12 @@
+operators = [
+'+', '-', '*', '/', '>', '<', '=', '<>', '<=', '>=', '<-', '&',
+"DECLARE", "OUTPUT", "INPUT", "IF", "ELSE", "CASE", "OTHERWISE", "FOR", "NEXT", "REPEAT", "UNTIL", "WHILE", "PROCEDURE", "CALL", "FUNCTION", "RETURN", "TYPE", "OPENFILE", "CLOSEFILE", "READFILE", "EOF", "WRITEFILE", "SEEK", "GETRECORD", "PUTRECORD", "CLASS", "PRIVATE", "PUBLIC",
+"RIGHT", "LENGTH", "MID", "LCASE", "UCASE", "INT", "RAND", "DIV", "MOD", "STEP"
+]
+
+guide_tokens = ["ENDIF", "ENDCASE", "ENDWHILE", "ENDPROCEDURE", "ENDFUNCTION", "ENDTYPE", "ENDCLASS", "THEN", "TO"]
+
+def get_user_def_operators(all_tokens: list):
+    for token in all_tokens:
+        if token == "FUNCTION" or token == "PROCEDURE":
+            operators.append(all_tokens[token + 1])
