e306a919b084e9c5e7a7b2e59bfac84655624e39
haspryt <haspryt@riseup.net>
2022-03-06 16:23:20 +0200

    created form tree function

--- form_tree.py
+++ form_tree.py
@@ -10,3 +10,49 @@ def get_user_def_operators(all_tokens: list):
     for token in all_tokens:
         if token == "FUNCTION" or token == "PROCEDURE":
             operators.append(all_tokens[token + 1])
+
+
+def get_arg(index: int, all_tokens: list):
+    match all_tokens[index]:
+        case '(':
+            saved_index = index
+            p_counter = 1
+            while p_counter > 0:
+                index += 1
+                match all_tokens[index]:
+                    case '(':
+                        p_counter += 1
+                    case ')':
+                        p_counter -= 1
+            return form_tree(all_tokens[slice(saved_index + 1, index)])
+        case ')':
+            saved_index = index
+            p_counter = 1
+            while p_counter > 0:
+                index += 1
+                match all_tokens[index]:
+                    case ')':
+                        p_counter += 1
+                    case '(':
+                        p_counter -= 1
+            return form_tree(all_tokens[slice(index + 1, saved_index)])
+        case _:
+            return all_tokens[index]
+
+
+def form_tree(all_tokens: list):
+    operators = get_user_def_operators(all_tokens)
+    for (index, token) in enumerate(all_tokens):
+        if token in operators:
+            current_operator = token
+            match token:
+                case '+'| '-' | '*' | '/' | '>' | '<' | '=' | '<>' | '<=' | '>=' | '<-' | '&' | "AND" | "NOT" | "OR":
+                    args = (get_arg(index - 1, all_tokens), get_arg(index + 1, all_tokens))
+                    return (token, args)
+                case "IF":
+                    saved_index = index
+                    current = token
+                    while current != "THEN":
+                        index += 1
+                        current = all_tokens[index]
+                    condition = form_tree(all_tokens[slice(saved_index + 1, index)])
